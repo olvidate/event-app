5aec2d04f33d87d08672ec19825b03325ffe38c2
olvidate <benjamin.ortizcl@gmail.com>
2023-12-12 20:43:30 -0300

    feat(backend): create model for Event entity

--- backend/eventApp/models.py
+++ backend/eventApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    # Título del evento (Varchar, máximo 100 caracteres)
+    title = models.CharField(max_length=100)
+    # Título del evento (Text, puede permitir valores nulos)
+    # (del caso contrario pediria un valor por defecto)
+    description = models.TextField(blank=True, null=True)
+    # Fecha del evento (DateTime)
+    date = models.DateTimeField()
+    # Ubicación del evento (Varchar, máximo 200 caracteres)
+    location = models.CharField(max_length=200)
+
+    # Método para mostrar el título en el
+    # panel de administración de Django.
+    def __str__(self):
+        return self.title
