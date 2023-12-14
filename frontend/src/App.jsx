76729c99ea9e44bdea0d4ed4e8ad891a03457220
olvidate <benjamin.ortizcl@gmail.com>
2023-12-14 03:38:03 -0300

    feat(frontend): create routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { EventsPage } from './pages/EventsPage';
+
 function App() {
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={< EventsPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App
