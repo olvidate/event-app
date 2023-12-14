ff42ccb552294ffcff6c45abd540e95546693b57
olvidate <benjamin.ortizcl@gmail.com>
2023-12-14 00:39:54 -0300

    fix(backend/settings): add cors whitelist to frontend

--- backend/DESAFIO_BLACKNEXUS/settings.py
+++ backend/DESAFIO_BLACKNEXUS/settings.py
@@ -127,4 +127,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Cors auth whitelist
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:5174"]
