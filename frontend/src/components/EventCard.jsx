ba8808aac2140f1b818d26fdf4794f1799ad5d81
olvidate <benjamin.ortizcl@gmail.com>
2023-12-15 22:34:46 -0300

    feat(frontend): use moment instead JS date

--- frontend/src/components/EventCard.jsx
+++ frontend/src/components/EventCard.jsx
@@ -1,10 +1,12 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './EventCard.css';
+import { deleteEvent } from '../api/events.api';
+import moment from 'moment';
 
 export function EventCard({ event }) {
-    
-    const date = new Date(event.date);
-    const formattedDate = date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear();
-
+    const navigate = useNavigate();
+    const formattedDate = moment(event.date).format('DD/MM/YYYY HH:mm');
     return (
         <section>
             <h2>{event.title}</h2>
@@ -14,8 +16,17 @@ export function EventCard({ event }) {
                 <p>Ubicación: {event.location}</p>
             </div>
             <div className="btn-separator">
-                <button>Editar</button>
-                <button>Borrar</button>
+                <button onClick={()=> {
+                    navigate('/events/edit/' + event.id);
+                }}>Editar</button>
+                <button onClick={async ()=> {
+                    const accepted = window.confirm('¿Estás seguro de borrar el evento?\nEsta acción no se puede deshacer');
+                    if(accepted) {
+                        await deleteEvent(event.id);
+                        console.log('borrar')
+                    };
+                    navigate('/');
+                }}>Borrar</button>
             </div>
         </section>
     );
