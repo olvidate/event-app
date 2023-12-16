c586fd8ab399ff5a3214d239253acc66b985e9eb
olvidate <benjamin.ortizcl@gmail.com>
2023-12-15 22:35:14 -0300

    feat(frontend): improve design into EventsPage

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,12 +1,20 @@
 import { EventList } from "../components/EventList";
 import './EventsPage.css';
 
+import { useNavigate } from 'react-router-dom';
+
 export function EventsPage() {
+    const navigate = useNavigate();
     return(
         <main>
             <header>
                 <h1>Eventos</h1>
                 <span>🥳</span>
+                <a onClick={()=> {
+                    navigate('/events/create');
+                }}>
+                    Crear Evento
+                </a>
             </header>
             <EventList></EventList>
         </main>
