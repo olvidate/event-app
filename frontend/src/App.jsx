f31de7776ed63b0a082aa359f003bcc22775c064
olvidate <benjamin.ortizcl@gmail.com>
2023-12-15 22:34:13 -0300

    feat(frontend): add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { EventsPage } from './pages/EventsPage';
+import { EventsFormPage } from './pages/EventsFormPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={< EventsPage />}></Route>
+        <Route path='/' element={<Navigate to='/events'/>}></Route>
+        <Route path='/events' element={< EventsPage />}></Route>
+        <Route path='/events/edit/:id' element={< EventsFormPage />}></Route>
+        <Route path='/events/create' element={< EventsFormPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
